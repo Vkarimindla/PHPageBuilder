4a731e9bbb0e4d3fc7b593e377328114ef29fef5
Vkarimindla <varshith401@gmail.com>
2024-07-09 10:51:03 +0530

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>7c6508e7 - MD5 Cracker</title>
+</head>
+<body>
+    <h1>Welcome to MD5 Cracker</h1>
+    <p>This application will crack an MD5 hash of a 4-digit PIN.</p>
+    <p>
+        <a href="crack.php">Go to the MD5 Cracker</a>
+    </p>
+</body>
+</html>
